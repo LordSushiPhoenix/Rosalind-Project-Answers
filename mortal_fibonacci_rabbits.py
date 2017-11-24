55bd73ba65ccbf3b3d9893f9bb39625a5e86e492
lordsushiphoenix <19640845+LordSushiPhoenix@users.noreply.github.com>
2017-11-24 15:33:40 -0500

    recurrence

---
+++ mortal_fibonacci_rabbits.py
@@ -0,0 +1,18 @@
+def mortFib(n, k):
+    if n <= 0:
+        return 0;
+    if n == 1:
+        return 1
+    #if n == 2:
+       #return 1
+    if n < k:
+       return (mortFib((n - 1), k) + mortFib((n - 2), k))
+    elif n == (k + 1):
+        return (mortFib((n - 2), k) + mortFib((n - 3), k) - mortFib((n-(k+1)), k) -1)
+    else:
+        return (mortFib((n - 2), k) + mortFib((n - 3), k) - mortFib((n-(k+1)), k))
+
+n = int(input('Enter the month value: '))
+k = int(input('Enter the life value: '))
+res = mortFib(n, k)
+print('Result is: %d' % res)
