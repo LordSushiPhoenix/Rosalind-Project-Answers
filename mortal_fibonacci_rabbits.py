f0c32ba42095b87b70f521b38906b646452b4cb4
ERIK-DESKTOP\Erik <erik.christensen108@gmail.com>
2017-11-25 08:10:23 -0500

    S P E E D B O I

--- mortal_fibonacci_rabbits.py
+++ mortal_fibonacci_rabbits.py
@@ -1,3 +1,5 @@
+'''
+#exponential complexity recursion yay
 def mortFib(n, k):
     if n <= 2:
         return 1
@@ -11,4 +13,29 @@ def mortFib(n, k):
 n = int(input('Enter the month value: '))
 k = int(input('Enter the life value: '))
 res = mortFib(n, k)
+print('Result is: %d' % res)
+'''
+#S P E E D B O I
+def mortFib(n, k, ar):
+    i = 0
+    while i < n:
+        #print(i)
+        if i <= 1:
+            ar.append(1)
+        elif i < k:
+            ar.append(ar[i - 1] + ar[i - 2])
+        elif i == (k):
+            ar.append(ar[i - 1] + ar[i - 2] - 1)
+        else:
+            ar.append(ar[i - 1] + ar[i - 2] - ar[i - (k + 1)])
+        i += 1
+    #print('list built')
+    #for j in ar:
+    #    print(j)
+    return ar[n-1]
+
+n = int(input('Enter the month value: '))
+k = int(input('Enter the life value: '))
+pop = []
+res = mortFib(n, k, pop)
 print('Result is: %d' % res)
