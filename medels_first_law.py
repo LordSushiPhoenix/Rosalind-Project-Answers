68d634b7e1f6a90008561f6e62ddfb89d1044556
ERIK-DESKTOP\Erik <erik.christensen108@gmail.com>
2017-11-25 08:36:51 -0500

    working on probability tree

---
+++ medels_first_law.py
@@ -0,0 +1,11 @@
+def probDom(k, m, n):
+    
+
+def main():
+    hd = int(input("Homozygous dominant: "))
+    h = int(input("Heterozygous: "))
+    hr = int(input("Homozygous recessive: "))
+    res = probDom(hd, h, hr)
+    print(res)
+
+main()
